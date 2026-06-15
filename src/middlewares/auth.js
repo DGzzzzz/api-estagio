4afe90f226b01827388df7a265416c0c025ee109
DGzzzzz <douglaswinter75@gmail.com>
2026-06-15 13:52:06 -0300

    alterar mensagens de retorno

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -1,24 +1,21 @@
 import jwt from 'jsonwebtoken';
 import authConfig from '../config/auth.js';
 
-export default async (req, res, next) => {
-
+export default (req, res, next) => {
     const authHeader = req.headers.authorization;
 
     if (!authHeader) {
-        return res.status(401).json({ error: 'Token does not exist.' });
+        return res.status(401).json({ error: 'Token não encontrado.' });
     }
-
-    //console.log('authHeader', authHeader);
 
     const [, token] = authHeader.split(' ');
 
     try {
-        const decoded = await jwt.verify(token, authConfig.secret);
+        const decoded = jwt.verify(token, authConfig.secret);
         req.userId = decoded.id;
+        req.userRole = decoded.role;
         return next();
-
-    } catch (error) {
-        return res.status(401).json({ error: 'Invalid token.' });
+    } catch {
+        return res.status(401).json({ error: 'Token inválido.' });
     }
 }
